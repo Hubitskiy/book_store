c34bffefa99e316a2c737a3056d2465c5af5c4eb
Hubitskiy <dmitriyhubitskiy@gmail.com>
2020-01-17 23:32:16 +0300

    add book models

--- src/books_heading/admin.py
+++ src/books_heading/admin.py
@@ -1,3 +1,11 @@
+from .models import Book
 from django.contrib import admin
 
+
+class AdminBook(admin.ModelAdmin):
+
+    list_display = ['title', 'author', 'genre', 'book_cover']
+
+
+admin.site.register(Book, AdminBook)
 # Register your models here.
--- src/books_heading/models.py
+++ src/books_heading/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils.safestring import mark_safe
+
+
+class Book(models.Model):
+    title = models.CharField(verbose_name='Название книги', max_length=30)
+    BOOKS_GENRES = [
+        ('NOVEL', 'Роман' ),
+        ('ADVENTURE', 'Приключенчиская литература'),
+        ('SATIRE', 'Сатира'),
+        ('DETECTIVE', 'Детектив'),
+        ('DRAMA', 'Драма')
+    ]
+    genre = models.CharField(verbose_name='Жанр книги', max_length=30, choices=BOOKS_GENRES)
+    author = models.CharField(verbose_name='Автор книги', max_length=30)
+    date_release = models.DateField(verbose_name='Дата издания')
+    language = models.SlugField(verbose_name='Язык книги')
+    cover = models.URLField(verbose_name='Обложка книги')
+    description = models.TextField(verbose_name='Описание книги')
+
+
+    def __str__(self):
+        return f'{self.title} - {self.author}'
+
+    def book_cover(self):
+        return mark_safe(f'<img> src="{self.cover}" width="100" height="100"/> ')
+
+
+# class Genre(models.Model):
+#     genre = models.CharField(verbose_name='Жанр книги', max_length=30, choices=)
 
 # Create your models here.
